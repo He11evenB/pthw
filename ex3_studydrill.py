bad316843cd311b2268fa06b4c307cff74e22f17
Josh Low <e11evenb@gmail.com>
2017-11-15 22:01:48 -0700

    Third example Study Drill with Python the Hard Way

---
+++ ex3_studydrill.py
@@ -0,0 +1,33 @@
+# prints to the screen
+print("I will now count my chickens:")
+
+# does the math and shows the results
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+# prints to the screen
+print("Now I will count the eggs:")
+
+# prints the result of the calculations
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# prints to the screen
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# prints the boolean of either True or False
+print(3 + 2 < 5 - 7)
+
+# prints the result of each calculation
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# prints to the screen
+print("Oh, that's why it's False.")
+
+# prints to the screen
+print("How about some more.")
+
+# prints the boolean of either True or False
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
