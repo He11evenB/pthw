24b20c7cb4b5b20aa1f6b912a908dfdebbef3ef5
Josh Low <e11evenb@gmail.com>
2017-11-16 22:49:41 -0700

    Fifth example Study Drill with Python the Hard Way

---
+++ ex5_studydrill.py
@@ -0,0 +1,22 @@
+name = 'Josh D. Low'
+age = 34
+height = 71
+weight = 230
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+kilo = weight * .45
+cm = height * 2.45
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} heair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+print(f"His weight in kilograms {kilo}.")
+print(f"His height in centimeters {cm}.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}")
